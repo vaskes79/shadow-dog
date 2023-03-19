0278d1839fabeacc06be4fc82219b2e8d192a8a2
Vasily Guzov <vasily.guzov@gmail.com>
2023-03-19 19:03:56 +0300

    [ui] add controll for change type of animation character

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,26 @@
+import { ChangeEvent, useRef } from 'react'
 import styles from './App.module.css';
+import type { Game, AnimationStateName } from '../Game'
+
+const optionsNames: AnimationStateName[] = [
+  'idle', 'jump', 'ko', 'run', 'fall', 'bite', 'dizzy', 'gethit', 'sit', 'roll'
+];
 
 export function App() {
+  const refContainer = useRef<HTMLDivElement>(null);
+  const options = optionsNames.map(item => <option key={item} value={item}>{item.toUpperCase()}</option>)
+
+  const handleChangeSelect = (event: ChangeEvent<HTMLSelectElement>) => {
+    const game = refContainer.current?.querySelector('ypr-game') as Game;
+    const { value } = event.target;
+    game.action = value as AnimationStateName;
+  }
 
   return (
-    <div className={styles.app}>
-      <h1>App</h1>
+    <div className={styles.app} ref={refContainer}>
+      <select onChange={handleChangeSelect} name="animation">
+        {options}
+      </select>
       <ypr-game></ypr-game>
     </div>
   )
